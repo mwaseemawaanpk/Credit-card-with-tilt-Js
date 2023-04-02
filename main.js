cf70f50ec695101a34c92a090608dea309349be0
Muhammad <waseemawanpk@gmail.com>
2023-04-03 00:25:53 +0500

    Credit card widget

---
+++ main.js
@@ -0,0 +1,9 @@
+const tilt = $('.debit-card').tilt({
+    maxTilt:        15,
+    easing:         "cubic-bezier(.03,.98,.52,.99)",
+    speed:          500, 
+    glare:         true,
+    perspective:    1500, 
+    maxGlare:       0.4,
+}
+);
